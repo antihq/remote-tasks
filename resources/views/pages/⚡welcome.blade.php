2d86959e42141893f3dd61c50d1e130a84d3b8a9
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-03-06 20:10:43 -0600

    init

---
+++ resources/views/pages/⚡welcome.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div class="min-h-screen flex flex-col items-center justify-center p-8">
+    <!--  -->
+</div>
