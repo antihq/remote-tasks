9e6a8ea4d19378f2669efb919a944aeda0a7859f
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-03-07 18:59:18 -0600

    wip

--- resources/views/scripts/callback.blade.php
+++ resources/views/scripts/callback.blade.php
@@ -8,14 +8,14 @@ cat > {{ $tempScriptPath }} << '{{ $token }}'
 {{ $token }}
 
 # Execute the user's script
+set +e
 @if ($task->timeout > 0)
 timeout {{ $task->timeout }}s bash {{ $tempScriptPath }}
 @else
 bash {{ $tempScriptPath }}
 @endif
-
-# Capture exit code
 EXIT_CODE=$?
+set -e
 
 # Callback to home server with exit code
 curl -X POST "{!! $callbackUrl !!}" -d "exit_code=${EXIT_CODE}" > /dev/null 2>&1 &
